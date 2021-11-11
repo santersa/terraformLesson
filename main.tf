28258fd1467e06a043eb4dc31f8f8318ca7d6148
santersa <syauchekolo@meta.ua>
2021-11-11 11:48:26 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,284 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a49b025fffbbdac6"
+  instance_type = "t2.micro"
+
+   tags = {
+     Name = var.instance_name
+   }
+
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name                             = "${var.namespace}-vpc"
+  cidr                             = "10.0.0.0/16"
+  azs                              = data.aws_availability_zones.available.names
+  private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
+  #assign_generated_ipv6_cidr_block = true
+  create_database_subnet_group     = true
+  enable_nat_gateway               = false
+  single_nat_gateway               = false
+}
+
+
+############################
+module "nat" {
+  source = "int128/nat-instance/aws"
+
+  name                        = "main"
+  vpc_id                      = module.vpc.vpc_id
+  public_subnet               = module.vpc.public_subnets[0]
+  private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  private_route_table_ids     = module.vpc.private_route_table_ids
+}
+
+resource "aws_eip" "nat" {
+  network_interface = module.nat.eni_id
+  tags = {
+    "Name" = "nat-instance-main"
+  }
+}
+
+
+// SG to allow SSH connections from anywhere
+resource "aws_security_group" "allow_ssh_pub" {
+  name        = "${var.namespace}-allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-allow_ssh_pub"
+  }
+}
+
+// SG to onlly allow SSH connections from VPC public subnets
+resource "aws_security_group" "allow_ssh_priv" {
+  name        = "${var.namespace}-allow_ssh_priv"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress {
+    description = "SSH only from internal VPC clients"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-allow_ssh_priv"
+  }
+}
+
+##########################
+### достаем subnet_id используя VPC ID
+
+data "aws_subnet_ids" "example" {
+
+# нужно ввести ID только что созданной VPC, а как вытащить ИД по имени не могу найти
+# стандартные модули Терраформ вытаскивают все опираясь на ИД
+
+  vpc_id = "vpc-014ac24e1eb3f6d44" 
+}
+
+data "aws_subnet" "example" {
+  for_each = data.aws_subnet_ids.example.ids
+  id       = each.value
+}
+
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.example : s.cidr_block]
+}
+
+output "subnet_id" {
+  value = [for s in data.aws_subnet.example : s.id]
+}
+
+
+#########################
+
+  module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 4.0"
+
+  # Autoscaling group
+  name = "example-asg"
+
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+
+# нужно подсунуть subnet_id из созданной VPC. Скрипт выше это делает, но ему нужен  VPC ID
+
+  vpc_zone_identifier       = ["subnet-1235678", "subnet-87654321"]
+
+  initial_lifecycle_hooks = [
+    {
+      name                  = "ExampleStartupLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 60
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                  = "ExampleTerminationLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 180
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+  ]
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
+
+  # Launch template
+  lt_name                = "example-asg"
+  description            = "Launch template example"
+  update_default_version = true
+
+  use_lt    = true
+  create_lt = true
+
+  image_id          = "ami-0a49b025fffbbdac6"
+  instance_type     = "t3.micro"
+  ebs_optimized     = true
+  enable_monitoring = true
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+      }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  capacity_reservation_specification = {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  credit_specification = {
+    cpu_credits = "standard"
+  }
+
+  instance_market_options = {
+    market_type = "spot"
+    spot_options = {
+      block_duration_minutes = 60
+    }
+  }
+
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 32
+  }
+
+  network_interfaces = [
+    {
+      delete_on_termination = true
+      description           = "eth0"
+      device_index          = 0
+      security_groups       = ["sg-0583b441922733d78"]
+    },
+    {
+      delete_on_termination = true
+      description           = "eth1"
+      device_index          = 1
+      security_groups       = ["sg-0583b441922733d78"]
+    }
+  ]
+
+  placement = {
+    availability_zone = "eu-central-1"
+  }
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = { WhatAmI = "Instance" }
+    },
+    {
+      resource_type = "volume"
+      tags          = { WhatAmI = "Volume" }
+    },
+    {
+      resource_type = "spot-instances-request"
+      tags          = { WhatAmI = "SpotInstanceRequest" }
+    }
+  ]
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
+
+  tags_as_map = {
+    extra_tag1 = "extra_value1"
+    extra_tag2 = "extra_value2"
+  }
+}
+
+##########################
+
